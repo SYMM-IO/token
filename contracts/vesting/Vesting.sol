a2cebb2de4184dc309e6d621b59ece5d1177d4c0
PacificOceanSailor <pacificoceansailor@gmail.com>
2025-02-11 00:22:45 +0330

    Change it to role based

--- contracts/vesting/Vesting.sol
+++ contracts/vesting/Vesting.sol
@@ -1,10 +1,12 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.8.27;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "../token/symm.sol";
 
-contract Vesting is Ownable{
+contract Vesting is AccessControlEnumerable{
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+
     mapping (address => uint256) public tokenAmounts;
     mapping (address => uint256) public claimedAmounts;
 
@@ -12,15 +14,17 @@ contract Vesting is Ownable{
     uint256 public immutable startTime;
     address public immutable symmAddress;
 
-    constructor(uint256 _totalTime, uint256 _startTime, address _symmTime, address owner) Ownable(owner){
+    constructor(uint256 _totalTime, uint256 _startTime, address _symmTime, address admin){
         require(block.timestamp >= startTime, "Start time should be grater or equal to now");
         totalTime = _totalTime;
         startTime = _startTime;
         symmAddress = _symmTime;
+        _grantRole(DEFAULT_ADMIN_ROLE, admin);
+        _grantRole(ADMIN_ROLE, admin); //Check: should they be different
     }
 
 
-    function setTokenAmount(address user, uint256 amount) external onlyOwner{
+    function setTokenAmount(address user, uint256 amount) external onlyRole(ADMIN_ROLE){
         require(amount >= claimedAmounts[user], "Requested amount exceeds claimed amount of the user");
         tokenAmounts[user] = amount;
     }
@@ -33,7 +37,7 @@ contract Vesting is Ownable{
         Symmio(symmAddress).mint(msg.sender, amount);
     }
 
-    function claimFor(uint256 amount, address user) external onlyOwner{ //Check should we have some amount that can be freed without time
+    function claimFor(uint256 amount, address user) external onlyRole(ADMIN_ROLE){ //Check should we have some amount that can be freed without time
         uint256 availableAmount = getAvailableAmount(user);
         require(availableAmount >= amount, "Requested amount exceeds available amount possible to claim");
         claimedAmounts[user] += amount; //Check the compiler version should avoid O.F.
