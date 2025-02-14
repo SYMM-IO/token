e39d5374d354b027fe2fe96d57e1e149f0454b8b
PacificOceanSailor <pacificoceansailor@gmail.com>
2025-02-14 16:22:36 +0330

    Add and edit vesting

--- tasks/Vesting.ts
+++ tasks/Vesting.ts
@@ -2,15 +2,15 @@ import { task } from "hardhat/config";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
 task("deploy:Vesting", "Deploys the Vesting contract")
-	.addParam("owner", "The owner of the Vesting contract")
+	.addParam("admin", "The admin of the Vesting contract")
 	.addParam("totalTime", "The total vesting duration in seconds")
 	.addParam("startTime", "The start time of the vesting period in UNIX timestamp")
 	.addParam("symmAddress", "The address of the Symmio token contract")
-	.setAction(async ({owner, totalTime, startTime, symmAddress }, { ethers, upgrades }: HardhatRuntimeEnvironment) => {
+	.setAction(async ({admin, totalTime, startTime, symmAddress }, { ethers, upgrades }: HardhatRuntimeEnvironment) => {
 		console.log("deploy:Vesting");
 
 		const VestingFactory = await ethers.getContractFactory("Vesting");
-		const vestingContract = await upgrades.deployProxy(VestingFactory, [owner, totalTime, startTime, symmAddress], { initializer: "initialize" });
+		const vestingContract = await upgrades.deployProxy(VestingFactory, [admin, totalTime, startTime, symmAddress], { initializer: "initialize" });
 		await vestingContract.waitForDeployment();
 
 		console.log(`Vesting Contract deployed at: ${await vestingContract.getAddress()}`);
