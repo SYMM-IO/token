94ee258809647794e4b3594a061150583cf09476
ZigBalthazar <zigbalthazar@gmail.com>
2025-02-19 21:58:44 +0330

    fix deploy vesting

--- tests/main.ts
+++ tests/main.ts
@@ -1,16 +1,21 @@
+import { shouldBehaveLikeAirdropHelper } from "./AirdropHelper.behavior"
 import { shouldBehaveLikeSymmAllocationClaimer } from "./symmAllocationClaimer.bhavior"
 import { shouldBehaveLikeSymmioToken } from "./symmioToken.behavior"
 import { ShouldBehaveLikeVesting } from "./vesting.behavior"
 
 describe("Symmio Token", () => {
 	describe("Static Tests", async function () {
-		// describe("Symm token", async function () {
-		// 	shouldBehaveLikeSymmioToken()
-		// })
+		describe("Symm token", async function () {
+			shouldBehaveLikeSymmioToken()
+		})
 
-		// describe("Allocation Claimer", async function () {
-		// 	shouldBehaveLikeSymmAllocationClaimer()
-		// })
+		describe("Allocation Claimer", async function () {
+			shouldBehaveLikeSymmAllocationClaimer()
+		})
+
+		describe("AirdropHelper", async function () {
+			shouldBehaveLikeAirdropHelper()
+		})
 
 		describe("ٰVesting", async function () {
 			ShouldBehaveLikeVesting()
