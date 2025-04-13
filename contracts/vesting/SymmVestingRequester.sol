08dd9d1d8ca540d37dc32c973926c7c108531b2d
PacificOceanSailor <pacificoceansailor@gmail.com>
2025-04-13 15:11:11 +0330

    Add inheritance

--- contracts/vesting/SymmVestingRequester.sol
+++ contracts/vesting/SymmVestingRequester.sol
@@ -1,17 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.18;
 
+import "@openzeppelin/contracts-upgradeable/access/extensions/AccessControlEnumerableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
 import {IVesting} from "./interfaces/IVesting.sol";
 
-contract SymmVestingRequester {
+contract SymmVestingRequester is Initializable, AccessControlEnumerableUpgradeable, PausableUpgradeable {
 
     error MismatchedArrays();
     error ZeroAmount();
 
-    //init
+    bytes32 public constant SETTER_ROLE = keccak256("SETTER_ROLE");
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant UNPAUSER_ROLE = keccak256("UNPAUSER_ROLE");
 
     uint256 public totalTime = 180 days;
-    uint256 public penaltyPerDay = 25e16;
+    uint256 public penaltyPerDay = 25e16; // 0.25e15
     uint256 public launchTime;
     uint256 public totalRegisteredAmount = 0;
     uint256 public totalRegisteredUsers = 0;
@@ -20,7 +24,20 @@ contract SymmVestingRequester {
     address public symmAddress;
     address public symmVestingAddress;
 
-    function registerPlans(address[] memory users, uint256[] memory amounts) external {//onlyRole
+    function initializer(address admin, address _symmAddress, address _symmVestingAddress) public initializer{
+        __AccessControlEnumerable_init();
+        __Pausable_init();
+
+        _grantRole(SETTER_ROLE, admin);
+        _grantRole(PAUSER_ROLE, admin);
+        _grantRole(UNPAUSER_ROLE, admin);
+
+        symmAddress = _symmAddress;
+        symmVestingAddress = _symmVestingAddress;
+        launchTime = block.timestamp;
+    }
+
+    function registerPlans(address[] memory users, uint256[] memory amounts) external onlyRole(SETTER_ROLE) { //TODO: whenNotPaused?
         if(users.length != amounts.length)
             revert MismatchedArrays();
         for(uint32 i=0; i<users.length; i++){
@@ -33,7 +50,7 @@ contract SymmVestingRequester {
         }
     }
 
-    function requestVestingPlan() external {
+    function requestVestingPlan() external whenNotPaused{
         if(registeredAmounts[msg.sender] == 0) revert ZeroAmount();
         address[] memory users;
         uint256[] memory amounts;
@@ -48,6 +65,14 @@ contract SymmVestingRequester {
         );
         totalVestedAmount += registeredAmounts[msg.sender];
         registeredAmounts[msg.sender] = 0;
+    }
+
+    function pause() external onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+
+    function unpause() external onlyRole(UNPAUSER_ROLE) {
+        _unpause();
     }
 
     function _getEndTime() private view returns(uint256){
