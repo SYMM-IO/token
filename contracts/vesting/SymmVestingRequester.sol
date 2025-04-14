b268cc0a83235fb5abb5409995fd4d615bd76dce
PacificOceanSailor <pacificoceansailor@gmail.com>
2025-04-14 14:52:39 +0330

    Change names

--- contracts/vesting/SymmVestingRequester.sol
+++ contracts/vesting/SymmVestingRequester.sol
@@ -14,17 +14,20 @@ contract SymmVestingRequester is AccessControlEnumerable, Pausable{
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant UNPAUSER_ROLE = keccak256("UNPAUSER_ROLE");
 
-    uint256 public totalDays = 180 days;
-    uint256 public penaltyPerDay = 25e16; // 0.25e15
-    uint256 public launchDay;
-    uint256 public totalRegisteredAmount = 0;
-    uint256 public totalRegisteredUsers = 0;
-    uint256 public totalVestedAmount = 0;
-    mapping(address=>uint256) public registeredAmounts; // user => amount
-    address public symmAddress;
-    address public symmVestingAddress;
+    uint256 public constant TOTAL_DAYS = 180 days;
+    uint256 public constant PENALTY_PER_DAY = 25e16; // 0.25e15
 
-    constructor(address admin, address _symmAddress, address _symmVestingAddress){
+    uint256 public immutable launchDay;
+    uint256 public immutable maxSymmAmount;
+    address public immutable symmAddress;
+    address public immutable symmVestingAddress;
+
+    uint256 public initiatableAmountsSum = 0;
+    uint256 public usersInitiatedCount = 0;
+    uint256 public totalVestedAmount = 0; //TODO: totalInitiatedAmount?
+    mapping(address=>uint256) public initiatableAmount; // user => amount //TODO: Can be renamed to pendingVestingPlan
+
+    constructor(address admin, address _symmAddress, address _symmVestingAddress, uint256 _totalInitiatableSYMM){
         _grantRole(SETTER_ROLE, admin);
         _grantRole(PAUSER_ROLE, admin);
         _grantRole(UNPAUSER_ROLE, admin);
@@ -32,27 +35,28 @@ contract SymmVestingRequester is AccessControlEnumerable, Pausable{
         symmAddress = _symmAddress;
         symmVestingAddress = _symmVestingAddress;
         launchDay = (block.timestamp / 1 days) * 1 days;
+        maxSymmAmount = _totalInitiatableSYMM;
     }
 
-    function registerPlans(address[] memory users, uint256[] memory amounts) external onlyRole(SETTER_ROLE) { //TODO: whenNotPaused?
+    function setInitiatableVestingAmount(address[] memory users, uint256[] memory amounts) external onlyRole(SETTER_ROLE) { //TODO: whenNotPaused?
         if(users.length != amounts.length)
             revert MismatchedArrays();
         for(uint32 i=0; i<users.length; i++){
-            if(registeredAmounts[users[i]] > amounts[i])
-                totalRegisteredAmount -= registeredAmounts[users[i]] - amounts[i];
+            if(initiatableAmount[users[i]] > amounts[i])
+                initiatableAmountsSum -= initiatableAmount[users[i]] - amounts[i];
             else
-                totalRegisteredAmount += amounts[i] - registeredAmounts[users[i]];
-            if(registeredAmounts[users[i]]!=0) totalRegisteredUsers += 1;//TODO: How to check it's not already added? add them to a mapping?!
-            registeredAmounts[users[i]] = amounts[i];
+                initiatableAmountsSum += amounts[i] - initiatableAmount[users[i]];
+            if(initiatableAmount[users[i]]!=0) usersInitiatedCount += 1;//TODO: How to check it's not already added? add them to a mapping?!
+            initiatableAmount[users[i]] = amounts[i];
         }
     }
 
-    function requestVestingPlan() external whenNotPaused{
-        if(registeredAmounts[msg.sender] == 0) revert ZeroAmount();
+    function initiateVestingPlan() external whenNotPaused {
+        if(initiatableAmount[msg.sender] == 0) revert ZeroAmount();
         address[] memory users;
         uint256[] memory amounts;
         users[0] = msg.sender;
-        amounts[0] = registeredAmounts[msg.sender];
+        amounts[0] = initiatableAmount[msg.sender];
         IVesting(symmVestingAddress).setupVestingPlans(
             symmAddress,
             block.timestamp,
@@ -60,8 +64,9 @@ contract SymmVestingRequester is AccessControlEnumerable, Pausable{
             users,
             amounts
         );
-        totalVestedAmount += registeredAmounts[msg.sender];
-        registeredAmounts[msg.sender] = 0;
+        totalVestedAmount += initiatableAmount[msg.sender];
+        if(totalVestedAmount > maxSymmAmount)
+        initiatableAmount[msg.sender] = 0;
     }
 
     function pause() external onlyRole(PAUSER_ROLE) {
@@ -75,7 +80,7 @@ contract SymmVestingRequester is AccessControlEnumerable, Pausable{
     function getEndTime() public view returns(uint256){
         uint256 today = (block.timestamp / 1 days) * 1 days;
         uint256 daysPassed = today - launchDay;
-        if(daysPassed > totalDays) daysPassed = totalDays;
-        return today + totalDays - daysPassed + daysPassed * penaltyPerDay / 1e18;
+        if(daysPassed > TOTAL_DAYS) daysPassed = TOTAL_DAYS;
+        return today + TOTAL_DAYS - daysPassed + daysPassed * PENALTY_PER_DAY / 1e18;
     }
 }
